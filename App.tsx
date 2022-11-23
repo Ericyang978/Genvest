f859d78def72a4da0aa944ebecdeb3bd2f8eb74d
ericy978 <elyang@berkeley.edu>
2022-11-23 12:23:35 -0800

    First round changes

--- App.tsx
+++ App.tsx
@@ -1,14 +1,108 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { COLOR_ACCENT, COLOR_PRIMARY } from "./AppStyles";
+import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail } from "firebase/auth";
+import React, { useState } from 'react';
+
+import { Appbar, TextInput, Snackbar, Button } from "react-native-paper";
+import { getApps, initializeApp } from 'firebase/app';
+
+const firebaseConfig = require("./firebasekeys.json")
+
+
+if (getApps().length == 0) {
+  initializeApp(firebaseConfig);
+}
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: COLOR_PRIMARY,
+    accent: COLOR_ACCENT,
+  },
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [appError, setAppError] = useState<String>("")
+  const [visible, setVisible] = useState<boolean>(false)
+
+  const auth = getAuth();
+
+  // creates account
+  const createAcccount = async () => {
+
+   try{
+    const userCredential = await createUserWithEmailAndPassword(auth, username, password)
+    console.log(userCredential.user)
+   } 
+    catch(error:any) {
+      setAppError(error.code.replace('auth/', '').replace('-', ' '));
+      setVisible(true);
+      // const errorCode = error.code;
+      // const errorMessage = error.message;
+      // ..
+  };
 }
+
+// Code for SnackBar (from docs)
+const onToggleSnackBar = () => setVisible(!visible);
+
+const onDismissSnackBar = () => setVisible(false);
+
+  return (
+    <>
+      <SafeAreaView>
+
+        <Appbar.Header>
+         {/* <Appbar.BackAction onPress={() => {}} /> */}
+         <Appbar.Content title="Create an Account" />
+        </Appbar.Header>
+      
+      <Text> </Text>
+        <TextInput
+          label="Email"
+          value={username}
+          onChangeText={text => setUsername(text)}
+        />
+         <Text> </Text> 
+         <TextInput
+          label="password"
+          value={password}
+          secureTextEntry={true}
+          onChangeText={text => setPassword(text)}
+        /> 
+        
+         <Text> </Text> 
+        <Button  mode="contained" onPress={createAcccount}>
+         create account
+        </Button>
+
+
+        <Text> </Text> 
+        {/* <Button  mode="contained" onPress={() => navigation.navigate("SignInScreen")}>
+          Sign In
+        </Button> */}
+
+        <Snackbar
+          duration={1000}
+          visible={visible}
+          onDismiss={onDismissSnackBar}
+        >
+         {appError}
+      </Snackbar>
+
+
+      </SafeAreaView>
+    </>
+  );
+
+}
+
 
 const styles = StyleSheet.create({
   container: {
