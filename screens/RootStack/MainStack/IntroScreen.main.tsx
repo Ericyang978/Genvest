047075ea7adf0578834111a1a0920fb199d8c42d
Amol Budhiraja <abudhiraja@berkeley.edu>
2022-11-27 21:27:15 -0800

    Started Home Page + Stock API Connection

--- screens/RootStack/MainStack/IntroScreen.main.tsx
+++ screens/RootStack/MainStack/IntroScreen.main.tsx
@@ -4,9 +4,8 @@ import { StackNavigationProp } from "@react-navigation/stack";
 import { Appbar, TextInput, Snackbar, Button } from "react-native-paper";
 import { MainStackParamList } from "./MainStackScreen";
 import { getAuth, signOut } from "firebase/auth";
-
-
-
+import { LinearGradient } from 'expo-linear-gradient';
+import axios from 'axios';
 
 interface Props {
   navigation: StackNavigationProp<MainStackParamList, "IntroScreen">;
@@ -32,18 +31,27 @@ export default function IntroScreen({ navigation }: Props) {
   }
 
 
-
   return(
-    <SafeAreaView>
+    <>
+      <PortfolioHeader />
+      <View style={{padding: 20}}><Text style={{fontWeight: 'bold', fontSize: 18}}>Top Movers</Text></View>
+      <View style={{padding: 20}}><Text style={{fontWeight: 'bold', fontSize: 18}}>My Stocks</Text></View>
+    </>
+  );
+}
 
-    
-
-     <Button mode="contained" onPress={logout} > 
-        go back
-     </Button>
-
-
-    </SafeAreaView>
-
+const PortfolioHeader = () => {
+  return (
+    <LinearGradient colors={["darkgreen", "green", "lightgreen"]} style={{borderRadius: 20, shadowOpacity: 0.8, paddingTop: 60, paddingBottom: 50, shadowRadius: 20}}>
+      <View style={{borderRadius: 20, shadowRadius: 20, shadowOpacity: 0.8, paddingVertical: 45}}>
+        <SafeAreaView>
+          <View style={{paddingHorizontal: 20}}>
+            <Text style={{color: "white", fontWeight: 'bold', fontSize: 20}}>Genvest</Text>
+            <Text style={{color: "white", fontSize: 45, fontWeight: 'bold', fontStyle: 'italic'}}>$299,375.25</Text>
+            <Text style={{color: "white"}}>Balance Available</Text>
+            </View>
+        </SafeAreaView>
+      </View>
+   </LinearGradient>
   );
 }
